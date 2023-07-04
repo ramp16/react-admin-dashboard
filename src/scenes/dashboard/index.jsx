2eb480991943ce6b300cb497b2d97a587e59cdfe
Raul Martinez <ra916mp@gmail.com>
2023-07-04 12:22:03 -0500

    avances del desarrollo del dashboard de admins en react con javascript, datagrid y material UI

---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
+const Dashboard = () => {
+    return <Box m="20px">Dashboard
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+    </Box>
+};
+
+export default Dashboard;

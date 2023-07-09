7f477aabd80ea29ede043fe449ced21ca14de7f4
Raul Martinez <ra916mp@gmail.com>
2023-07-09 16:13:24 -0500

    se agrego el codigo final para los componentes barchart, geographychart, linechart, piechart, progresschart, y statbox; ademas sus respectivas carpetas dentro de la carpeta de sceness

--- src/App.js
+++ src/App.js
@@ -7,38 +7,38 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
 import Form from "./scenes/form";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
+import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar";
-//import { useState } from "react";
+import { useState } from "react";
 
 function App() {
   const [theme, colorMode] = useMode(); // hook para el light y dark mode
-  //const [isSidebar ,setIsSidebar] = useState(true);
+  const [isSidebar ,setIsSidebar] = useState(true);
   
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme = {theme}>
         <CssBaseline />
         <div className="app">
-          <Sidebar/>
+          <Sidebar isSidebar={isSidebar} />
           <main className="content">
-            <Topbar /*setIsSidebar={setIsSidebar}*/ />
+            <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard/>} />
               <Route path="/team" element={<Team/>} />
               <Route path="/invoices" element={<Invoices/>} />
               <Route path="/contacts" element={<Contacts/>} />
-              {/* <Route path="/bar" element={<Bar/>} /> */}
+              <Route path="/bar" element={<Bar/>} />
               <Route path="/form" element={<Form/>} />
-              {/* <Route path="/line" element={<Line/>} /> */}
-              {/* <Route path="/pie" element={<Pie/>} /> */}
+              <Route path="/line" element={<Line/>} />
+              <Route path="/pie" element={<Pie/>} />
               <Route path="/faq" element={<FAQ/>} />
-              {/* <Route path="/geography" element={<Geography/>} /> */}
+              <Route path="/geography" element={<Geography/>} />
               <Route path="/calendar" element={<Calendar/>} />
             </Routes>
           </main>

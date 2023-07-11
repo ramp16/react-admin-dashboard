31ab85067afc8a08fff7d8a71a32df207ba36baf
Raul Martinez <ra916mp@gmail.com>
2023-07-11 12:00:17 -0500

    correccion de error en linea de codigo de la pagina de formulario de creacion de nuevo usuario

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -13,7 +13,7 @@ const Form = () => {
 
     return (
         <Box m="20px">
-            <Header title="CREATE USER" subtitles="Create a New User Profile" />
+            <Header title="CREATE USER" subtitle="Create a New User Profile" />
 
             <Formik
                 onSubmit={handleFormSubmit}
